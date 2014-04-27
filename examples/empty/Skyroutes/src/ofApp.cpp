b93f4e1049920fccd2fb30b3ee19c1b26ad36e06
Kaiying Liao <kygliao@gmail.com>
2014-04-27 12:52:12 -0400

    added objects, demonstrated moving sphere

--- examples/empty/Skyroutes/src/ofApp.cpp
+++ examples/empty/Skyroutes/src/ofApp.cpp
@@ -1,8 +1,19 @@
 #include "ofApp.h"
 
+int valx;
+int valy;
+int valz;
+int j;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+	//ofSetFrameRate(500);
+
+	valx = 500;
+	valy = 300;
+	valz = 10;
+	j = 5;
 }
 
 //--------------------------------------------------------------
@@ -13,6 +24,22 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+	valz += j;
+	if (valz > 500){
+		j = -5;
+	}
+	else if (valz < 0){
+		j = 5;
+	}
+	
+	ofSetColor(255,0,255);
+	//ofCircle(valx, valy, 100);
+	ofDrawSphere(valx, valy, valz, 30);
+
+	ofSetColor(100, 100, 255);
+	ofDrawBox(200, 400, 0, 40, 300, 50);
+
+	ofDrawBox(800, 500, 0, 80, 200, 50);
 }
 
 //--------------------------------------------------------------
